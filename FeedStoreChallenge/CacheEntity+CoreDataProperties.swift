77d3050da366a3bf63fb509487696342067df237
Mo Al Waili <mohammed.alwaili@gmail.com>
2021-03-02 15:46:00 +0100

    remove auto generated extension functions in the CacheEntity

--- FeedStoreChallenge/CacheEntity+CoreDataProperties.swift
+++ FeedStoreChallenge/CacheEntity+CoreDataProperties.swift
@@ -21,41 +21,6 @@ extension CacheEntity {
 
 }
 
-// MARK: Generated accessors for feed
 extension CacheEntity {
-
-    @objc(insertObject:inFeedAtIndex:)
-    @NSManaged func insertIntoFeed(_ value: LocalFeedImageEntity, at idx: Int)
-
-    @objc(removeObjectFromFeedAtIndex:)
-    @NSManaged func removeFromFeed(at idx: Int)
-
-    @objc(insertFeed:atIndexes:)
-    @NSManaged func insertIntoFeed(_ values: [LocalFeedImageEntity], at indexes: NSIndexSet)
-
-    @objc(removeFeedAtIndexes:)
-    @NSManaged func removeFromFeed(at indexes: NSIndexSet)
-
-    @objc(replaceObjectInFeedAtIndex:withObject:)
-    @NSManaged func replaceFeed(at idx: Int, with value: LocalFeedImageEntity)
-
-    @objc(replaceFeedAtIndexes:withFeed:)
-    @NSManaged func replaceFeed(at indexes: NSIndexSet, with values: [LocalFeedImageEntity])
-
-    @objc(addFeedObject:)
-    @NSManaged func addToFeed(_ value: LocalFeedImageEntity)
-
-    @objc(removeFeedObject:)
-    @NSManaged func removeFromFeed(_ value: LocalFeedImageEntity)
-
-    @objc(addFeed:)
-    @NSManaged func addToFeed(_ values: NSOrderedSet)
-
-    @objc(removeFeed:)
-    @NSManaged func removeFromFeed(_ values: NSOrderedSet)
-
-}
-
-extension CacheEntity : Identifiable {
 
 }
